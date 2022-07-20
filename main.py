fc12d713ae04b9212a290433422652fff1cf56fa
Ayesha Amjad <81661022+Ayesha21410@users.noreply.github.com>
2022-07-20 03:53:03 +0000

    put asked to append question

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk # for Images
 from tkinter import messagebox  # for error messages
 import random
 names_list = [] 
+asked=[] #List used to append question.
 #Component 1
 class QuizStarter:
     def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
